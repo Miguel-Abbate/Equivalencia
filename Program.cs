dcdf03e00955e6ddc0df3c463f0b3336b23681a5
Miguel-Abbate <abbatejr@gmail.com>
2025-09-27 09:48:02 -0300

    2° versão

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿double metro;
-Console.WriteLine("Escreva um valor em metros:");
-double.TryParse(Console.ReadLine(), out metro);
+﻿Console.WriteLine("Escreva um valor em metros:");
+string entrada = Console.ReadLine()!;
+double metro = Convert.ToDouble(entrada);
 double kilometro = metro /1000;
 double centimetro = metro * 100;
 Console.Clear();
- Console.ForegroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine($"{kilometro}Km");
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"{metro}m");
